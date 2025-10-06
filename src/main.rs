0ae9578d50a15ea7432fa1583d7f5fa18b211c7d
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2025-10-06 16:59:42 +0800

    feat: title generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use arboard::Clipboard;
 use clap::Parser;
-use lipsum::{lipsum, lipsum_words_with_rng};
+use lipsum::{lipsum, lipsum_title_with_rng, lipsum_words_with_rng};
 use rand::thread_rng;
 use std::{borrow::Cow, fmt::Write, thread::sleep, time::Duration};
 
@@ -20,6 +20,9 @@ struct Cli {
     /// Disable clipboard addition integration.
     #[arg(long, default_value_t = false)]
     pub no_clipboard: bool,
+    /// Title generation mode.
+    #[arg(long, short, default_value_t = false)]
+    pub title: bool,
 }
 
 fn dummy_text(paragraph: usize, word: usize, newline: bool) -> String {
@@ -38,6 +41,10 @@ fn dummy_text(paragraph: usize, word: usize, newline: bool) -> String {
     text
 }
 
+fn dummy_title() -> String {
+    lipsum_title_with_rng(thread_rng())
+}
+
 fn add_to_clipboard<'a>(text: impl Into<Cow<'a, str>>) -> anyhow::Result<()> {
     let mut clip = Clipboard::new()?;
     clip.set_text(text)?;
@@ -47,7 +54,11 @@ fn add_to_clipboard<'a>(text: impl Into<Cow<'a, str>>) -> anyhow::Result<()> {
 
 fn main() {
     let cli = Cli::parse();
-    let text = dummy_text(cli.paragraph, cli.word, !cli.no_newline);
+    let text = if cli.title {
+        dummy_title()
+    } else {
+        dummy_text(cli.paragraph, cli.word, !cli.no_newline)
+    };
     println!("{text}");
     if !cli.no_clipboard {
         match add_to_clipboard(text) {
